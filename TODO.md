4ab22223fc8a3d3c89db3087073c8aa94e205ae8
copandrej <ac8560@student.uni-lj.si>
2022-09-12 15:51:43 +0200

    added some things, broke some things

---
+++ TODO.md
@@ -0,0 +1,15 @@
+# TODO
+
+- next events fix -- done
+- footer changes -- done
+- Fix footer mobile  -- recimo
+- change small meni background to black -- done
+- add discover more
+- mobile notes/events distance fix
+- Fix mobile cards at events and notes pages
+- Fix image sizing at events and notes pages on mobile
+- Add a configurator bar to notes page
+- iphone paralex fix
+- add responsive design for all dimensions
+- Change/refactor the code for mobile first design
+- Clean up the code
--- js/script.js
+++ js/script.js
@@ -1,7 +1,43 @@
 function anime() {
-    document.getElementById("sprememba").classList.toggle("spremenia");
+	document.getElementById("sprememba").classList.toggle("spremenia");
 	document.getElementById("spremembaa").classList.toggle("spremeni");
 	document.getElementById("spremembaaa").classList.toggle("spremeniaa");
 	document.getElementById("mobile-meni").classList.toggle("display-none");
-	
+
 }
+
+//from: https://coolcssanimation.com/element-fade-out-on-scroll/
+const header = document.getElementById('fade-out');
+const knof = document.getElementById('knof');
+let disp = true;
+
+function fadeOutOnScroll(element) {
+	if (!element) {
+		return;
+	}
+
+	let opacity = 1;
+	opacity = 1 - Math.max(window.pageYOffset - 300, 0) / 300;
+
+	if (!disp) {
+		element.style.visibility = 'visible';
+		knof.style.visibility = 'visible';
+		disp = false;
+	}
+
+	if (opacity >= 0) {
+		element.style.opacity = opacity;
+
+	} else {
+		element.style.opacity = 0;
+		element.style.visibility = 'hidden';
+		knof.style.visibility = 'hidden';
+		disp = false;
+	}
+}
+
+function scrollHandler() {
+	fadeOutOnScroll(header);
+}
+
+window.addEventListener('scroll', scrollHandler);
