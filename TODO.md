c2ddaf3e85a50b57fc1924b78a392b5884c2713e
copandrej <ac8560@student.uni-lj.si>
2022-09-12 19:25:25 +0200

    more things fixed, more things broken

--- TODO.md
+++ TODO.md
@@ -4,12 +4,13 @@
 - footer changes -- done
 - Fix footer mobile  -- recimo
 - change small meni background to black -- done
-- add discover more
-- mobile notes/events distance fix
+- add discover more -- done - še funkcionalnost
+- mobile notes/events distance fix -- done
 - Fix mobile cards at events and notes pages
-- Fix image sizing at events and notes pages on mobile
-- Add a configurator bar to notes page
-- iphone paralex fix
+- notes fix mobile text possitioning 
+- Fix image sizing at events and notes pages on mobile -- done
+- Add a configurator bar to notes page -- še delovanje
+- iphone paralex fix 
 - add responsive design for all dimensions
 - Change/refactor the code for mobile first design
 - Clean up the code
--- js/script.js
+++ js/script.js
@@ -5,39 +5,3 @@ function anime() {
 	document.getElementById("mobile-meni").classList.toggle("display-none");
 
 }
-
-//from: https://coolcssanimation.com/element-fade-out-on-scroll/
-const header = document.getElementById('fade-out');
-const knof = document.getElementById('knof');
-let disp = true;
-
-function fadeOutOnScroll(element) {
-	if (!element) {
-		return;
-	}
-
-	let opacity = 1;
-	opacity = 1 - Math.max(window.pageYOffset - 300, 0) / 300;
-
-	if (!disp) {
-		element.style.visibility = 'visible';
-		knof.style.visibility = 'visible';
-		disp = false;
-	}
-
-	if (opacity >= 0) {
-		element.style.opacity = opacity;
-
-	} else {
-		element.style.opacity = 0;
-		element.style.visibility = 'hidden';
-		knof.style.visibility = 'hidden';
-		disp = false;
-	}
-}
-
-function scrollHandler() {
-	fadeOutOnScroll(header);
-}
-
-window.addEventListener('scroll', scrollHandler);
